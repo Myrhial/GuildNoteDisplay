f56efee9ff5533284efc47b810b9163bf023ae48
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-11-02 00:04:41 +0100

    When the characters' short name and note are identical, do not display the note because that's redundant

--- GuildNoteDisplay.lua
+++ GuildNoteDisplay.lua
@@ -1,7 +1,7 @@
 -- Gets the public note for a player by looping over the number of guild members
 local function FindPublicNoteForPlayer(nameWithRealm)
     for i = 1, GetNumGuildMembers() do
-        local name, rankName, rankIndex, level, classDisplayName, zone, publicNote = GetGuildRosterInfo(i)
+        local name, _, _, _, _, _, publicNote = GetGuildRosterInfo(i)
         if name == nameWithRealm then
             return publicNote
         end
@@ -11,9 +11,9 @@ end
 local function AddGuildNoteToGuildChat(self, event, msg, author, ...)
     if event == "CHAT_MSG_GUILD" then
         local publicNote = FindPublicNoteForPlayer(author)
+        local shortName = Ambiguate(author, "short")
 
-        -- TODO: Do not show if author and guild note are the same (but player name is with realm attached)
-        if publicNote and publicNote ~= "" then       
+        if publicNote and publicNote ~= "" and string.lower(shortName) ~= string.lower(publicNote) then       
             -- TODO: Appending the note to the message or to the name could be a choice. As part of the message it will have the guild color. As part of the author it will have the author class color. 
             --msg = "(" .. publicNote .. ") " .. msg
             -- INFO: Author is in the format of name-realm but in guild this would normally show as name only when on the same realm so we need to ambiguate it
