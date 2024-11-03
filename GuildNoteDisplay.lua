109a8dd201e8ac4c6a0c57cf82c4999e639b0aa3
Myrhial <8618272+ninaarens@users.noreply.github.com>
2024-11-03 23:58:31 +0100

    We can has settings! And a colour picker! So fancy now!

--- GuildNoteDisplay.lua
+++ GuildNoteDisplay.lua
@@ -1,5 +1,72 @@
+-- Initialisation
+local appName, app = ...    -- App name and app table
+app.api = {}    -- Api table for our app
+GuildNoteDisplay = app.api  -- Api namespace
+local api = app.api -- Api prefix for easier access
+app.Name = "Guild Note Display"
+
+-- Event registration
+local event = CreateFrame("Frame")
+event:SetScript("OnEvent", function(self, event, ...)
+	if self[event] then
+		self[event](self, ...)
+	end
+end)
+event:RegisterEvent("ADDON_LOADED")
+
+-- Initial load
+function app.Initialise()
+	-- Declare SavedVariables
+	if not GuildNoteDisplayDB then GuildNoteDisplayDB = {} end
+	
+	-- Enable default user settings
+	if GuildNoteDisplayDB["note_colour_table"] == nil then 
+        GuildNoteDisplayDB["note_colour_table"] = {
+			r=1,
+			g=1,
+			b=1,
+		}
+    end
+end
+
+-- Addon is loaded
+function event:ADDON_LOADED(addOnName, containsBindings)
+	if addOnName == appName then
+        app.Initialise()
+        app.Settings()
+    end
+end
+
+-- Color picker
+function app.ShowColorPicker(configTable)
+	local r, g, b, a = GuildNoteDisplayDB.note_colour_table.r, GuildNoteDisplayDB.note_colour_table.g, GuildNoteDisplayDB.note_colour_table.b, GuildNoteDisplayDB.note_colour_table.a;
+
+	local function OnColorChanged()
+		local newR, newG, newB = ColorPickerFrame:GetColorRGB();
+		local newA = ColorPickerFrame:GetColorAlpha();
+		GuildNoteDisplayDB.note_colour_table.r, GuildNoteDisplayDB.note_colour_table.g, GuildNoteDisplayDB.note_colour_table.b, GuildNoteDisplayDB.note_colour_table.a = newR, newG, newB, newA;
+	end
+
+	local function OnCancel()
+		GuildNoteDisplayDB.note_colour_table.r, GuildNoteDisplayDB.note_colour_table.g, GuildNoteDisplayDB.note_colour_table.b, GuildNoteDisplayDB.note_colour_table.a = r, g, b, a;
+	end
+
+	local options = {
+		swatchFunc = OnColorChanged,
+		opacityFunc = OnColorChanged,
+		cancelFunc = OnCancel,
+		hasOpacity = a ~= nil,
+		opacity = a,
+		r = r,
+		g = g,
+		b = b,
+	};
+
+	ColorPickerFrame:SetupColorPickerAndShow(options);
+end
+
 -- Gets the public note for a player by looping over the number of guild members
-local function FindPublicNoteForPlayer(nameWithRealm)
+function app.FindPublicNoteForPlayer(nameWithRealm)
     for i = 1, GetNumGuildMembers() do
         local name, _, _, _, _, _, publicNote = GetGuildRosterInfo(i)
         if name == nameWithRealm then
@@ -8,14 +75,15 @@ local function FindPublicNoteForPlayer(nameWithRealm)
     end
 end
 
-local function AddGuildNoteToGuildChat(self, event, msg, author, ...)
+-- Adds the public note to the guild chat
+function app.AddGuildNoteToGuildChat(self, event, msg, author, ...)
     if event == "CHAT_MSG_GUILD" then
-        local publicNote = FindPublicNoteForPlayer(author)
+        local publicNote = app.FindPublicNoteForPlayer(author)
         local shortName = Ambiguate(author, "short")
 
         if publicNote and publicNote ~= "" and string.lower(shortName) ~= string.lower(publicNote) then
-            -- TODO: Color picker for the note text
-            msg = "|cFFFFFFFF(" .. publicNote .. ")|r " .. msg
+            local textColor = CreateColor(GuildNoteDisplayDB.note_colour_table.r, GuildNoteDisplayDB.note_colour_table.g, GuildNoteDisplayDB.note_colour_table.b, GuildNoteDisplayDB.note_colour_table.a);
+            msg = "|c" .. textColor:GenerateHexColor() .. "(" .. publicNote .. ")|r " .. msg
             -- TODO: This method is going to become opt-in via a setting so we can warn about what happens when you click on the name to whipser (note is placed in the whisper)
             -- INFO: Author is in the format of name-realm but in guild this would normally show as name only when on the same realm so we need to ambiguate it
             -- author = Ambiguate(author, "guild") .. " (" .. publicNote .. ")"
@@ -24,4 +92,24 @@ local function AddGuildNoteToGuildChat(self, event, msg, author, ...)
     end
 end
 
-ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", AddGuildNoteToGuildChat)
+-- Add the event filter
+ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", app.AddGuildNoteToGuildChat)
+
+-- Settings
+function app.Settings()
+	-- Settings page
+	local category, layout = Settings.RegisterVerticalLayoutCategory(app.Name)
+	Settings.RegisterAddOnCategory(category)
+	app.Category = category
+
+	layout:AddInitializer(CreateSettingsListSectionHeaderInitializer(C_AddOns.GetAddOnMetadata(appName, "Version")))
+
+    do -- color picker
+        local function OnButtonClick()
+            app.ShowColorPicker(GuildNoteDisplayDB.note_colour_table)
+        end
+
+        local initializer = CreateSettingsButtonInitializer("Guild note colour", "Click to pick a colour", OnButtonClick, nil, true);
+        layout:AddInitializer(initializer);
+    end
+end
